b370e644309c5626b2e5062703dcea700d24b983
nindyashivakhairunnisa <nindyashivakhairunnisa@gmail.com>
2025-01-22 11:49:20 +0700

    fgnfhn

---
+++ latihan1.java
@@ -0,0 +1,15 @@
+package com.belajar;
+
+public class latihan1 {
+    public static void main(String[] args) {
+        String nama = "Nindya";
+        String kelas = "X PPLG 1";
+        double B_badan = 42.00;
+        System.out.println("nama saya : " +nama);
+        System.out.println("kelas : ");
+        System.out.println("berat badan : ");
+        System.out.println(B_badan);
+    }
+
+
+
---
+++ latihan4.java
@@ -0,0 +1,31 @@
+package com.belajar;
+import java.util.Scanner;
+
+public interface Latihan4 {
+    public static void main(String[] args) {
+        //Mengolah Input Data Sederhana 
+        //Menggunakan Class Scanner
+        int NISN, Usia;
+        String Nama, Alamat;
+        Scanner Input = new Scanner(System.in);
+        System.out.println("DATA SISWA");
+        System.out.println("------------");
+        System.out.println("NISN");
+        NISN = Input.nextInt();
+        System.out.println("Nama : ");
+        Nama = Input.next();
+        System.out.println("Alamat : ");
+        Alamat = Input.next();
+        System.out.println("Usia : ");
+        Usia = Input.nextInt();
+        //Menamoilkan Data Siswa
+        System.out.println("DATA SISWA");
+        System.out.println("------------");
+        System.out.println("NISN : + NISN");
+        System.out.println("Nama : " + Nama);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("Usia : " + Usia);
+        
+    }
+    
+}
