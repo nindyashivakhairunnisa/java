a4b766ee1554139a99161f818df5693944b3fc66
nindyashivakhairunnisa <nindyashivakhairunnisa@gmail.com>
2025-05-21 12:22:40 +0700

    Add files via upload

---
+++ bukuSiswa.java
@@ -0,0 +1,12 @@
+
+
+public class bukuSiswa extends buku {
+        public static void main(String[] args) {
+            buku modul = new buku();
+            System.out.println(modul.judul);
+            System.out.println(modul.penerbit);
+            modul.kategori();    
+        }
+    }
+    
+
