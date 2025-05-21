dc9b335e923b5561cfde03c1a551f0dd9de66376
nindyashivakhairunnisa <nindyashivakhairunnisa@gmail.com>
2025-05-21 12:17:51 +0700

    Add files via upload

---
+++ latihan6.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JOptionPane;
+public class latihan6 {
+   public static void main(String[] args) {
+    String nama = "";
+    nama = JOptionPane.showInputDialog("Silahkan masukkan nama anda");
+    String msg = "Hello " + nama + "!";
+    JOptionPane.showMessageDialog(null, msg);
+   } 
+}
